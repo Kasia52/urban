46fab6b8945e535e022b9531771ec6abc0fa1d44
Kasia52 <john.barett666@gmail.com>
2024-10-09 21:55:14 +0300

    commit

---
+++ main.py
@@ -0,0 +1,35 @@
+# This is a sample Python script.
+from operator import truediv
+
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# задача 1 арифметика
+print("1st program")
+print( 9 ** 0.5 * 5)
+# задача 2
+print("2nd program")
+print( 9.99 > 9.98 and 1000 != 1000.1)
+# задача 3
+print("3rd program")
+a=2 * 2 + 2
+b=2 * (2+2)
+print(a)
+print(b)
+print(a == b)
+#задача 4
+print("4th program")
+c= float('123.456')
+print(int (c * 10 %10)  )
---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = 'Vadim'
+print('Name: ' + name)
+age = 24
+print(f'Age: {age}')
+age = age + 2
+print(f'New age: {age}')
+is_student = True
+print(f'Is student: {is_student}')
