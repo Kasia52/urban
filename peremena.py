43aef0227727cf7951a4c52e33ff0d858cbb97fd
Kasia52 <john.barett666@gmail.com>
2024-10-10 22:57:50 +0300

    commit

---
+++ peremena.py
@@ -0,0 +1,5 @@
+course_name = 'Python'
+count_homework = 12
+wasted_time = 1.5
+result = wasted_time / count_homework
+print(f'{course_name}, всего задач:{count_homework}, затрачено часов: {wasted_time}, среднее время выполнения {result}')
