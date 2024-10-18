39593f27a09e36bf5d25b7c0bbf61a03150c1279
Kasia52 <john.barett666@gmail.com>
2024-10-18 23:43:00 +0300

    where is цикл

---
+++ module_2_2.py
@@ -0,0 +1,19 @@
+first = 1
+second = 2
+third = 3
+if first == second == third:
+    print('salfetka')
+else:
+    print('bipbop')
+
+barry = 28
+john = 40.5
+antony = 33
+if john >= barry:
+    print('John is a better in battle then barry')
+if antony < john:
+    print('antony is upset')
+else:
+    print('barry always win')
+
+
